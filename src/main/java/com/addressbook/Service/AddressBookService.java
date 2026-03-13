e3b7b873424be792ac53b2a20f7648ba21b1802c
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2026-03-13 15:27:44 +0530

    [Anubhav Kumar Srivastava] Add. completed feature/UC1-ability-to-create-a-contact-in-addressbook

---
+++ src/main/java/com/addressbook/Service/AddressBookService.java
@@ -0,0 +1,49 @@
+package com.addressbook.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.addressbook.DTO.ContactDTO;
+import com.addressbook.Entity.Contact;
+
+@Service
+public class AddressBookService {
+	
+	List<Contact> contacts= new ArrayList<>();
+	 
+	public Contact addContact(ContactDTO contactDTO) {
+		Contact newContact= toEntity(contactDTO);
+		newContact.setId(contacts.size()+1);
+		contacts.add(newContact);
+		return newContact;
+	}
+ 
+	
+	private Contact toEntity(ContactDTO contactDTO) {
+		return new Contact(
+				contactDTO.getFirstName(),
+				contactDTO.getLastName(),
+				contactDTO.getAddress(),
+				contactDTO.getCity(),
+				contactDTO.getState(),
+				contactDTO.getZip(),
+				contactDTO.getPhoneNumber(),
+				contactDTO.getEmail()
+		);
+	}
+	
+	private ContactDTO toDTO(Contact contact) {
+		ContactDTO dto= new ContactDTO();
+		dto.setFirstName(contact.getFirstName());
+		dto.setLastName(contact.getLastName());
+		dto.setAddress(contact.getAddress());
+		dto.setCity(contact.getCity());
+		dto.setState(contact.getState());
+		dto.setZip(contact.getZip());
+		dto.setPhoneNumber(contact.getPhoneNumber());
+		dto.setEmail(contact.getEmail());
+		return dto;
+	}
+}
