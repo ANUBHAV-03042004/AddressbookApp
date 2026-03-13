5abc7ec3aa2c47896c854861211580de0921e449
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2026-03-13 22:41:31 +0530

    [Anubhav Kumar Srivastava] Refactored. Updated feature/UC1-ability-to-create-a-contact-in-addressbook

--- src/main/java/com/addressbook/Controller/AddressBookController.java
+++ src/main/java/com/addressbook/Controller/AddressBookController.java
@@ -1,29 +1,25 @@
 package com.addressbook.Controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.addressbook.DTO.ContactDTO;
-import com.addressbook.Entity.Contact;
-import com.addressbook.Service.AddressBookService;
+import com.addressbook.Entity.ContactEntity;
+import com.addressbook.Service.AddressbookService;
 @RestController
-public class AddressBookController {
+public class AddressbookController {
 
 	@Autowired
-	AddressBookService addressBookService;
+	AddressbookService addressbookService;
 	
 	@PostMapping("/addcontact")
-	public ResponseEntity<Contact> addContact(@RequestBody ContactDTO contactDTO) {
-		Contact newContact= addressBookService.addContact(contactDTO);
+	public ResponseEntity<ContactEntity> addContact(@RequestBody ContactDTO contactDTO) {
+		ContactEntity newContact= addressbookService.addContact(contactDTO);
 		return new ResponseEntity<>(newContact, HttpStatus.CREATED);
 	}
  
--- src/main/java/com/addressbook/Service/AddressBookService.java
+++ src/main/java/com/addressbook/Service/AddressBookService.java
@@ -6,23 +6,23 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.addressbook.DTO.ContactDTO;
-import com.addressbook.Entity.Contact;
+import com.addressbook.Entity.ContactEntity;
 
 @Service
-public class AddressBookService {
+public class AddressbookService {
 	
-	List<Contact> contacts= new ArrayList<>();
+	List<ContactEntity> contacts= new ArrayList<>();
 	 
-	public Contact addContact(ContactDTO contactDTO) {
-		Contact newContact= toEntity(contactDTO);
+	public ContactEntity addContact(ContactDTO contactDTO) {
+		ContactEntity newContact= toEntity(contactDTO);
 		newContact.setId(contacts.size()+1);
 		contacts.add(newContact);
 		return newContact;
 	}
  
 	
-	private Contact toEntity(ContactDTO contactDTO) {
-		return new Contact(
+	private ContactEntity toEntity(ContactDTO contactDTO) {
+		return new ContactEntity(
 				contactDTO.getFirstName(),
 				contactDTO.getLastName(),
 				contactDTO.getAddress(),
@@ -34,7 +34,7 @@ public class AddressBookService {
 		);
 	}
 	
-	private ContactDTO toDTO(Contact contact) {
+	private ContactDTO toDTO(ContactEntity contact) {
 		ContactDTO dto= new ContactDTO();
 		dto.setFirstName(contact.getFirstName());
 		dto.setLastName(contact.getLastName());
